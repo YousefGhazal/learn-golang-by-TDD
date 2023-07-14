5d22225fe783a97efa14aa28ef1ad245bb9cdfd2
Yousef <yousefghazal2412@gmail.com>
2023-07-15 02:17:54 +0300

    arrays

--- main.go
+++ main.go
@@ -2,49 +2,27 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	// basic swithc
-	i := 3
-	fmt.Print("Writ ", i, " as ")
-	switch i {
-	case 1:
-		fmt.Println("one")
-	case 2:
-		fmt.Println("two")
-	case 3:
-		fmt.Println("three")
-	}
-	//multiple expressions and default case 
-	switch time.Now().Weekday() {
-	case time.Saturday, time.Sunday:
-		fmt.Println("it's the weekend")
-	default:
-		fmt.Println("it's a weekday")
-	}
-	//switch without an expression is an alternate way to express if/else logic
-	t := time.Now()
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("it's before noon")
-	default:
-		fmt.Println("it's after noon")
 
-	}
-	whatAmI := func(i interface{}) {
-		switch t := i.(type) {
-		case bool:
-			fmt.Println("i'm a bool")
-		case int:
-			fmt.Println("i'm an int")
-		default:
-			fmt.Printf("Don't know type %T\n", t)
+	var a [5]int
+	fmt.Println("emp:", a)
+
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+
+	fmt.Println("len:", len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
+
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
 		}
 	}
-	whatAmI(true)
-	whatAmI(3)
-	whatAmI("hey")
-
+	fmt.Println("2d:", twoD)
 }
